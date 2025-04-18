257b2fa8f9557d6a0d8da1bae6f687a2b049bb48
Pedro <vieiraphs2@gmail.com>
2025-04-17 21:28:27 -0300

    Fix: Ajustando Autentificacao

--- project-lytax/src/auth/constants.ts
+++ project-lytax/src/auth/constants.ts
@@ -1,4 +1,5 @@
 import { ConfigService } from '@nestjs/config';
+import { SetMetadata } from '@nestjs/common';
 
 const configService = new ConfigService();
 
@@ -6,3 +7,6 @@ export const jwtConstants = {
   secret: configService.get<string>('JWT_SECRET'),
   expire: configService.get<string>('JWT_EXPIRE')
 };
+
+export const IS_PUBLIC_KEY = 'isPublic';
+export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);
